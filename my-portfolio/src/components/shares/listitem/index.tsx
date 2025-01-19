08c96b7c9a2056b800cde396e8b3489fd85d9393
nanairo2743 <136669608+nanairo2743@users.noreply.github.com>
2025-01-20 00:52:07 +0900

    fix:worksの部分の作成

---
+++ my-portfolio/src/components/shares/listitem/index.tsx
@@ -0,0 +1,47 @@
+import type { Dayjs } from 'dayjs';
+import type { ReactElement } from 'react';
+import Fontawesome from '../../shares/fontawesome';
+import dayjs from 'dayjs';
+import styles from './index.module.scss';
+
+export interface ListItemProps {
+  date: Dayjs | [Dayjs] | [Dayjs, Dayjs];
+  type: string;
+  title: string;
+  link?: string;
+  description?: string;
+  dateFormat?: string;
+}
+
+function ListItem(props: ListItemProps): ReactElement {
+  const { date, type, title, link, description, dateFormat } = props;
+  const dateFormatSnap = dateFormat ?? 'YYYY/MM/DD';
+
+  return (
+    <li className={styles.lit_item}>
+      <p className={styles.date}>
+        {Array.isArray(date)
+          ? `${dayjs(date[0]).format(dateFormatSnap)} ~ ${date[1] === undefined ? '現在' : dayjs(date[1]).format(dateFormatSnap)}`
+          : dayjs(date).format(dateFormatSnap)}
+      </p>
+
+      <p className={styles.main}>
+        <span>
+          [
+          {type}
+          ]
+        </span>
+        <span className={styles.title}>{title}</span>
+        {link !== undefined && (
+          <a className={styles.link} href={link}>
+            <Fontawesome className={styles.link_icon} iconKey="faUpRightFromSquare" />
+          </a>
+        )}
+      </p>
+
+      {description !== undefined && <p className={styles.description}>{description}</p>}
+    </li>
+  );
+}
+
+export default ListItem;
